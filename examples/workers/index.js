cff7eda72625f7c4b11ad5ab124fd74496b8d397
Shirley de Jong <shirleydejong@users.noreply.github.com>
2023-07-03 18:29:42 +0200

    Not needed

--- examples/workers/index.js
+++
@@ -1,7 +0,0 @@
-const { Worker } = require('worker_threads');
-const worker = new Worker(__dirname + '/worker.js');
-console.log('Log 1 from parent');
-
-setTimeout(()=>{
-    console.log('Log 2 from parent');
-},50)
--- examples/workers/worker.js
+++
@@ -1,5 +0,0 @@
-require('../../index')(console, { format: ':date(yyyy/mm/dd HH:MM:ss.l) :label' });
-console.log('Log 1 from worker');
-console.error('Error 1 from worker');
-console.log('Log 2 from worker');
-console.error('Error 2 from worker');
