62b322cc9bf6f21b02add49b12848b925e28846e
Shirley de Jong <shirleydejong@users.noreply.github.com>
2023-07-04 13:46:15 +0200

    Update date.js

--- tokens/date.js
+++ tokens/date.js
@@ -3,5 +3,5 @@ module.exports = ( { params: [format, utc = false, date = new Date] } ) => {
     if( utc ) {
         return DateTime.fromJSDate( date ).setZone('utc').toFormat( format ) 
     }
-    return DateTime.fromJSDate( date ).local().toFormat( format ) 
+    return DateTime.fromJSDate( date ).setZone('system').toFormat( format ) 
 }
